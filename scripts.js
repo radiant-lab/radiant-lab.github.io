1769d5412c0f0409ea6defcc510c0d430517fe41
radiant-lab <simon-kerner@posteo.com>
2024-09-12 20:20:50 +0200

    redesign with text boxes, added content

--- scripts.js
+++ scripts.js
@@ -6,3 +6,17 @@ document.addEventListener('DOMContentLoaded', () => {
         nav.classList.toggle('active');
     });
 });
+
+
+// Event listener for all thumbnails
+document.querySelectorAll('.thumbnail').forEach(thumbnail => {
+    thumbnail.addEventListener('click', function () {
+        const targetId = this.getAttribute('data-target');
+        const newImage = this.getAttribute('data-image');
+        const newDescription = this.getAttribute('data-description');
+
+        // Update the featured image and description
+        document.getElementById('featuredImage' + targetId).src = newImage;
+        document.getElementById('imageDescription' + targetId).textContent = newDescription;
+    });
+});
